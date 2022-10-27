3bc13ce5690835eadee1e07cc4ad16fb92bea7f5
SudeepRed <sudeep19313@iiitd.ac.in>
2022-10-27 22:06:44 +0530

    frontend init

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          FCS GROUP 15, October Milestone incoming!
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
