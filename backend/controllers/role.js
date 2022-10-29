7cfc1bc6aa82976af79f16b1f483be22b74b2c84
expnoob <sudeep19313@iiitd.ac.in>
2022-10-29 20:20:32 +0530

    fixed role

--- backend/controllers/role.js
+++ backend/controllers/role.js
@@ -1,6 +1,6 @@
 export function roleCheck(role) {
   return (req, res, next) => {
-    if (req.user.role != role) {
+    if (req.session.user.role != role) {
       res.status(401);
       return res.send("You dont have permission to access this!");
     }
